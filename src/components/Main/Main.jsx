7dce85fd8ef4d6fef6a9a50e3e1ae2538d124b5e
Konstantin Roy <konstantin.roy9977@gmail.com>
2023-03-15 17:10:28 +0300

    v

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -20,99 +20,69 @@ import QuotesMobPhoto from "./img/6. QuotesMobPhoto.jpg";
 function Main() {
   return (
     <>
-    <Navbar />
-    <div className={styles.mainBlock}>
-      <div className={`${styles.photoBlock} ${styles.mainPhotoBlock}`}>
-        <img className={styles.pcPhoto} src={PcMainPhoto} alt="" />
-        <img className={`${styles.mobPhoto} ${styles.mainMobPhoto}`} src={MobMainPhoto} alt="" />
-        <p className={styles.mainPhotoTextOne}>Величайшее</p>
-        <p className={styles.mainPhotoTextTwo}>противостояние</p>
-        <p className={styles.mainPhotoTextThree}>в истории</p>
-        <p className={styles.mainPhotoTextFour}>спорта</p>
+      <Navbar />
+      <div className={styles.mainBlock}>
+        <div className={`${styles.photoBlock} ${styles.mainPhotoBlock}`}>
+          <img className={styles.pcPhoto} src={PcMainPhoto} alt="" />
+          <img
+            className={`${styles.mobPhoto} ${styles.mainMobPhoto}`}
+            src={MobMainPhoto}
+            alt=""
+          />
+          <p className={styles.mainPhotoTextOne}>Величайшее</p>
+          <p className={styles.mainPhotoTextTwo}>противостояние</p>
+          <p className={styles.mainPhotoTextThree}>в истории</p>
+          <p className={styles.mainPhotoTextFour}>спорта</p>
+        </div>
+        <div className={styles.photoBlock}>
+          <img className={styles.pcPhoto} src={GoldenBallPc} alt="" />
+          <img className={styles.mobPhoto} src={GoldenBallMob} alt="" />
+          <p className={styles.photoText}>Расписание ближайших туров</p>
+        </div>
+        <div className={styles.photoBlock}>
+          <img className={styles.pcPhoto} src={ResultsPcPhoto} alt="" />
+          <img className={styles.mobPhoto} src={ResultsMobPhoto} alt="" />
+          <p className={styles.photoText}>
+            Результаты
+            <br /> всех матчей
+            <br /> сезона
+          </p>
+        </div>
+        <div className={styles.photoBlock}>
+          <img className={styles.pcPhoto} src={LocationPcPhoto} alt="" />
+          <img className={styles.mobPhoto} src={LocationMobPhoto} alt="" />
+          <p className={styles.photoText}>Места проведения каток</p>
+        </div>
+        <div className={styles.photoBlock}>
+          <img className={styles.pcPhoto} src={AudioPcPhoto} alt="" />
+          <img className={styles.mobPhoto} src={AudioMobPhoto} alt="" />
+          <p className={styles.photoText}>
+            Все аудио
+            <br /> в одном
+            <br /> месте
+          </p>
+        </div>
+        <div className={styles.photoBlock}>
+          <img className={styles.pcPhoto} src={PhotosPcPhoto} alt="" />
+          <img className={styles.mobPhoto} src={PhotosMobPhoto} alt="" />
+          <p className={styles.photoText}>
+            Лучшие
+            <br /> фоточки
+            <br /> за всё время
+          </p>
+        </div>
+        <div className={styles.photoBlock}>
+          <img className={styles.pcPhoto} src={QuotesPcPhoto} alt="" />
+          <img className={styles.mobPhoto} src={QuotesMobPhoto} alt="" />
+          <p className={styles.photoText}>
+            Лучшие
+            <br /> цитаты
+            <br /> Рая и Дудки
+          </p>
+        </div>
       </div>
-      <div className={styles.photoBlock}>
-        <img
-          className={styles.pcPhoto}
-          src={GoldenBallPc}
-          alt=""
-        />
-        <img
-          className={styles.mobPhoto}
-          src={GoldenBallMob}
-          alt=""
-        />
-        <p className={styles.photoText}>Расписание ближайших туров</p>
-      </div>
-      <div className={styles.photoBlock}>
-        <img
-          className={styles.pcPhoto}
-          src={ResultsPcPhoto}
-          alt=""
-        />
-        <img
-          className={styles.mobPhoto}
-          src={ResultsMobPhoto}
-          alt=""
-        />
-        <p className={styles.photoText}>Результаты<br /> всех матчей<br /> сезона</p>
-      </div>
-      <div className={styles.photoBlock}>
-        <img
-          className={styles.pcPhoto}
-          src={LocationPcPhoto}
-          alt=""
-        />
-        <img
-          className={styles.mobPhoto}
-          src={LocationMobPhoto}
-          alt=""
-        />
-        <p className={styles.photoText}>Места проведения каток</p>
-      </div>
-      <div className={styles.photoBlock}>
-        <img
-          className={styles.pcPhoto}
-          src={AudioPcPhoto}
-          alt=""
-        />
-        <img
-          className={styles.mobPhoto}
-          src={AudioMobPhoto}
-          alt=""
-        />
-        <p className={styles.photoText}>Все аудио<br /> в одном<br /> месте</p>
-      </div>
-      <div className={styles.photoBlock}>
-        <img
-          className={styles.pcPhoto}
-          src={PhotosPcPhoto}
-          alt=""
-        />
-        <img
-          className={styles.mobPhoto}
-          src={PhotosMobPhoto}
-          alt=""
-        />
-        <p className={styles.photoText}>Лучшие<br /> фоточки<br /> за всё время</p>
-      </div>
-      <div className={styles.photoBlock}>
-        <img
-          className={styles.pcPhoto}
-          src={QuotesPcPhoto}
-          alt=""
-        />
-        <img
-          className={styles.mobPhoto}
-          src={QuotesMobPhoto}
-          alt=""
-        />
-        <p className={styles.photoText}>Лучшие<br /> цитаты<br /> Рая и Дудки</p>
-      </div>
-    </div>
-    <Footer />
-      
+      <Footer />
     </>
-
   );
 }
 
